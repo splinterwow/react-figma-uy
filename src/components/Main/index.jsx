e1237d43714e9e725e61f4b3f250ab94dfc85ed7
splinterwow <asosiymenu007@gmail.com>
2024-04-25 11:25:45 +0500

    react-figma

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,9 @@
 import YouTubeImg from "../../../public/assets/youtube-embed.svg";
 import cardImg1 from "../../../public/assets/website-illustration.svg";
+import cardImg2 from "../../../public/assets/bot-illustration.svg";
+import cardImg3 from "../../../public/assets/smm-illustration.svg";
+import cardImg4 from "../../../public/assets/design-illustration.svg";
+import cardImg5 from "../../../public/assets/crm-illustration.svg";
 import styles from "../Main/index.module.css";
 
 function Main() {
@@ -12,7 +16,7 @@ function Main() {
             olib chiqing
           </h2>
           <p>
-            Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu.{" "}
+            Nullam laoreet nec turpis et ultrices. Duis sit amet quam arcu.
             <br /> Nam facilisis lacinia ex, eget sollicitudin massa
             pellentesque <br /> in. Vivamus mattis eros at sem pulvinar
           </p>
@@ -36,11 +40,36 @@ function Main() {
             <h3>Vebsayt tuzish</h3>
             <p>lorem ipsum</p>
           </div>
-          <div className={styles.card2}></div>
-          <div className={styles.card3}></div>
-          <div className={styles.card4}></div>
-          <div className={styles.card5}></div>
+          <div className={styles.card2}>
+            <img src={cardImg2} alt="" />
+            <h3>Telegram bot</h3>
+            <p>lorem ipsum</p>
+          </div>
+          <div className={styles.card3}>
+            <img src={cardImg3} alt="" />
+            <h3>SMM</h3>
+            <p>lorem ipsum</p>
+          </div>
         </div>
+        <br />
+        <div className={styles.mainCard}>
+          <div className={styles.card4}>
+            <img src={cardImg4} alt="" />
+            <h3>Grafik dizayn</h3>
+            <p>lorem ipsum</p>
+          </div>
+          <div className={styles.card5}>
+            <img src={cardImg5} alt="" />
+            <h3>CRM tizimlar</h3>
+            <p>lorem ipsum</p>
+          </div>
+        </div>
+        <button className={styles.btn3}>
+          <b>Buyurtma berish</b>
+        </button>
+        <a className={styles.sahifa} href="#">
+          Xizmatlar sahifasiga o’tish
+        </a>
       </div>
     </>
   );
